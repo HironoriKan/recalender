0166ce19664a64ae9b99686ef91171eef048e8a2
HironoriKan <HironoriKan>
2025-03-07 02:25:02 +0900

    UIの改善とVercel設定の修正

--- src/App.tsx
+++ src/App.tsx
@@ -5,11 +5,14 @@ function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <div className="App">
+    <div className="App w-full h-screen flex items-center justify-center">
       <header className="App-header">
-        <h1>RECALENDER</h1>
+        <h1 className="text-3xl font-bold mb-4">RECALENDER</h1>
         <p>
-          <button onClick={() => setCount((count) => count + 1)}>
+          <button 
+            className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+            onClick={() => setCount((count) => count + 1)}
+          >
             カウント: {count}
           </button>
         </p>
